f89100b9c39670a371d60c7951221c9479cff8b3
jingwei <56349014@qq.com>
2019-08-20 01:05:57 +0800

    解决价格模拟

--- 1600万验算.py
+++ 1600万验算.py
@@ -20,9 +20,14 @@ import random
 def goldPriceSimulation(days):
     goldPriceFluctionList = [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-    def getPrice(day,count):
+    def getPrice(count):
 
-        print("========第{0:}天========".format(day+1, count))
+        # 黄金价格波动随着日期增长，振幅扩大的概率会巨大提升。
+        # 当波动幅度超过14美金后，振幅的概率会重新从0 计算
+        # 黄金每20天内必然会发生产生20美金以上的振幅
+        # 每周有概率会产生一次12美金以上的振幅
+        # 每两周有概率会产生15美金以上的振幅
+        # 黄金每日的振幅随机，为5-9美金
         randomNumber = random.random()
         print("随机值为：{:.2f}".format(randomNumber))
 
@@ -30,59 +35,54 @@ def goldPriceSimulation(days):
         goldPriceFluctionRate = (len(goldPriceFluctionList) / 20) * count
 
         priceLimit = round(goldPriceFluctionRate)  # 随着时间增长，价格波动增大的概率会余越大
-
+        print("当前限制{}".format(priceLimit))
         if randomNumber < 0.8:
             if priceLimit < 5:
-                priceRange = goldPriceFluctionList[0:5]  # 当波动值不高时，黄金振幅为5-10美金
-                print('test')
+                priceRange = goldPriceFluctionList[0:6]  # 当波动值不高时，黄金振幅为5-10美金
 
             elif priceLimit < len(goldPriceFluctionList):
                 priceRange = goldPriceFluctionList[priceLimit:]
-                print('test2')
 
             elif priceLimit >= len(goldPriceFluctionList):# 当振幅概率超出列表数量时，限制最大值为列表最大值
-                print('test3')
                 priceRange = goldPriceFluctionList[len(goldPriceFluctionList)-1:len(goldPriceFluctionList)]
-            print("今日的价格波动范围是aa{}".format(priceRange))
+            # print("今日的价格波动范围是aa{}".format(priceRange))
         else:
             priceRange = goldPriceFluctionList # 有少许概率即使概率不高，振幅会不固定
-            print("今日的价格波动范围是bb{}".format(priceRange))
         # 获取随机价格
         price = random.choice(priceRange)
 
-        return price
+        return price, priceRange
 
 
-
-    for day in range(days):
-        # 黄金价格波动随着日期增长，振幅扩大的概率会巨大提升。
-        # 当波动幅度超过14美金后，振幅的概率会重新从0 计算
-        # 黄金每20天内必然会发生产生20美金以上的振幅
-        # 每周有概率会产生一次12美金以上的振幅
-        # 每两周有概率会产生15美金以上的振幅
-        # 黄金每日的振幅随机，为5-9美金
-
-
-        count = 1
-        price =getPrice(day,count)
-        if price < 14:
-
-            print("第{0:}天的价格波动为{1:}美金\n行情累积天数:{2:}".format(day+1, price,count))
+    count = 0
+    def rangeReset(count):
+    # 当振幅超过14美金时，将下一交易日的振幅限制到10美金一下
+        if getPrice(count)[0] > 14:
+            priceRange = [5,6,7,8,9,10]
+            return priceRange
         else:
-            count = 1
-            print("第{0:}天的价格波动为{1:}美金, \n行情累积天数:{2:}\n价格范围计数器将重置".format(day+1, price,count))
+            return getPrice(count)[1]
 
-        # if price > 14:
-        #     day = i - day
-        #     print("价格重置")
-        #
+    # 根据天数模拟天数的振幅
+    for day in range(days):
+        print("========第{0:}天========".format(day + 1))
+        print("当前计数: {}".format(count))
+        count += 1
+        if count != 0 :
+            price = getPrice(count)
+            if price[0] < 14:
+                print("今日的价格波动范围是{}".format(price[1]))
+                print("第{0:}天的价格波动为{1:}美金".format(day+1, price[0]))
+            else:
+                count = 0
+                print("今日的价格波动范围是{}".format(price[1]))
+                print("第{0:}天的价格波动为{1:}美金, 价格范围计数器将重置".format(day + 1, price[0]))
+    return price
 
 
 
-goldPriceSimulation(25)
 
-
-def goldCalc(init, successRate, days):
+def goldCalc(initialDeplosit, successRate, days):
     # 规则说明
     #
     # 黄金日内波动    账户盈亏        振幅概率
@@ -92,27 +92,33 @@ def goldCalc(init, successRate, days):
     # 15          300%          0.1
     # 20          650%          0.05
     #
+    minislot = 0.01
+    accountValue = initialDeplosit
+    prob = successRate
+    price = getPrice(days)
 
-    #
+
+
     # 设置黄金振幅与翻倍关系
     goldPriceAndAccountValue = {
-        '5': '100%',
-        '10': '150%',
-        '12': '200%',
-        '15': '300%',
-        '20': '650%'
+        '5':   '0.42',
+        '6':   '0.56',
+        '7':   '0.72',
+        '8':   '1.1',
+        '9':   '1.32',
+        '10':  '1.56',
+        '11':  '1.82',
+        '12':  '2.4',
+        '13':  '2.72',
+        '14':  '3.06',
+        '15':  '3.42',
+        '16':  '4.2',
+        '17':  '4.62',
+        '18':  '5.06',
+        '19':  '5.52',
+        '20':  '6.5',
+
     }
-
-
-
-
-    init = init * successRate
-    random.seed(2)
-
-    # while init < 100000:
-    print(random.Random)
-    print(random.randint(0,1))
-    print(random.randint(1,1001))
 
 
 def riskAsset(init):
@@ -123,5 +129,6 @@ def currencyInflation(rate):
 
 
 def main():
-    return ""
+    goldPriceSimulation(25)
 
+main()
